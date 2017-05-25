284651039c6838fe0544a41768ba906972af1fe2
Noelle Lewkowicz <noelle.lewkowicz@gmail.com>
2017-05-25 13:21:39 +0200

    checkpoint 22 comlete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-var points = document.getElementsByClassName('point');
-
+var animatePoints = function(points) {
 for (var i = 0; i < points.length; i++){
 points[i].style.opacity = 1;
 points[i].style.transform = "scaleX(1) translateY(0)";
@@ -10,4 +9,19 @@ points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
                };
 };
 
-animatePoints();
+
+window.onload = function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+   if (window.innerHeight > 950) {
+       animatePoints(pointsArray);
+   }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+    }
+    });
+ }
