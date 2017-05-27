67760fce2d3a0203f7924d05dafe6a2d958f2ea2
Noelle Lewkowicz <noelle.lewkowicz@gmail.com>
2017-05-27 10:53:46 +0200

    update 22

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point){
+point.style.opacity = 1;
+point.style.transform = "scaleX(1) translateY(0)";
+point.style.msTransform = "scaleX(1) translateY(0)";
+point.style.WebkitTransform = "scaleX(1) translateY(0)";
+                 };
+
 var animatePoints = function(points) {
-// for (var i = 0; i < points.length; i++){
-points[i].style.opacity = 1;
-points[i].style.transform = "scaleX(1) translateY(0)";
-points[i].style.msTransform = "scaleX(1) translateY(0)";
-points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-                // };
+  forEach(points, revealPoint);
 };
-forEach(pointsArray, animatePoints);
 
 
 window.onload = function() {
