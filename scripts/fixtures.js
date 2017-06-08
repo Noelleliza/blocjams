0bcee912a3eabe4aa1478e9a00894bc5c9bab31e
Noelle Lewkowicz <noelle.lewkowicz@gmail.com>
2017-06-08 11:19:58 +0200

    checkpoint 31

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumNoelle = {
+    title: 'Where to next?',
+    artist: 'N.E.L.',
+    label: 'Femme',
+    year: '1989',
+    albumArtUrl: 'assets/images/album_covers/nel.jpg',
+    songs: [
+        { title: 'Who I am', duration: '1:01' },
+        { title: 'Travel 2 Worlds', duration: '2:21' },
+        { title: 'Home sweet home', duration: '3:78'},
+        { title: 'Try that again', duration: '7:30' },
+        { title: 'I\'m back', duration: '20:17'}
+    ]
+};
